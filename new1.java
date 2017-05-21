34ee02b85682cdbbad4e3f0c1b3160c7b67ad7e8
Florian August <zibibednarek@gmail.com>
2017-05-21 19:08:11 +0200

    First comming new1

---
+++ new1.java
@@ -0,0 +1,21 @@
+/**
+ * Created by admin on 2017-05-21.
+ */
+public class new1 {
+    public static void main(String[] args) {
+
+        String myName = ("Zbigniew") ;
+
+        double eurCash = 300 ;
+        double usdCash = 200 ;
+        double plnCash = 400 ;
+
+        double eurPln = 4.20 ;
+        double usdPln = 3.80 ;
+        double totalCash = eurCash * eurPln + usdCash * usdPln + plnCash;
+        System.out.println("Hello " + myName + "! " + "You have " + totalCash + " PLN.");
+
+    }
+
+
+}
